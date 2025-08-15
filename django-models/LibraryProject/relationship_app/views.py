439357ed294c6dbf377dc0e08da581e8b033f329
Emmanuel <emmanuelkibe148@gmail.com>
2025-08-15 11:40:38 +0300

    changed admin_view to Admin

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -58,7 +58,7 @@ def logout_view(request):
     logout(request)
     return render(request, "relationship_app/logout.html")
 
-def Admin(user):
+def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 def is_librarian(user):
@@ -67,8 +67,8 @@ def is_librarian(user):
 def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
-@user_passes_test(Admin)
-def admin_view(request):
+@user_passes_test(is_admin)
+def Admin(request):
     return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
