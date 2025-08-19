a164bc786f915a97d7b9e655261e3b64ed863d56
Emmanuel <emmanuelkibe148@gmail.com>
2025-08-19 12:35:30 +0300

    Registered the Comment model

--- my_shop/my_app/admin.py
+++ my_shop/my_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Product
+from .models import Comment
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -10,3 +11,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ('price', 'stock')
     ordering = ('name',)
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'created_at', 'flagged')
+    search_fields = ('text',)
+    list_filter = ('flagged', 'created_at')
+    ordering = ('-created_at',)
