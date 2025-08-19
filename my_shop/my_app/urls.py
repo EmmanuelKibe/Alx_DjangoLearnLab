422bd1c496add2861a443612df101d04f3e69a20
Emmanuel <emmanuelkibe148@gmail.com>
2025-08-19 11:50:03 +0300

    Created routers that automatically generated urls for the viewsets

--- my_shop/my_app/urls.py
+++ my_shop/my_app/urls.py
@@ -1,6 +1,13 @@
-from .views import ProductListCreateAPIView
-from django.urls import path
+from .views import ProductListCreateAPIView, CommentViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('products/', ProductListCreateAPIView.as_view(), name='product-list-create'),
+    path('api/', include(router.urls)),
 ]
