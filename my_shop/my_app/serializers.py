8e028bd1419d1219b301c66adec42047c404055c
Emmanuel <emmanuelkibe148@gmail.com>
2025-08-19 12:36:00 +0300

    Added the 'days since created' logic

--- my_shop/my_app/serializers.py
+++ my_shop/my_app/serializers.py
@@ -15,6 +15,14 @@ class ProductSerializer(serializers.ModelSerializer):
         return delta.days
     
 class CommentSerializer(serializers.ModelSerializer):
+
+    days_since_created = serializers.SerializerMethodField()
+
     class Meta:
         model = Comment
         fields = '__all__'
+
+    def get_days_since_created(self, obj):
+        # obj is the Comment instance
+        delta = timezone.now() - obj.created_at
+        return delta.days
