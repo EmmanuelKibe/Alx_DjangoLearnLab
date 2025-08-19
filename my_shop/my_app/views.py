455719506beeb951194cec26886a9062a500cd9b
Emmanuel <emmanuelkibe148@gmail.com>
2025-08-19 11:49:27 +0300

    Created a viewset called CommentViewSet that provides all CRUD operations

--- my_shop/my_app/views.py
+++ my_shop/my_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Product
 from .serializers import ProductSerializer
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
