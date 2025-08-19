9bfb9499ce1612dd4c129b88ceb87177b2517033
Emmanuel <emmanuelkibe148@gmail.com>
2025-08-19 12:41:32 +0300

    Actions to mark flagged comments

--- my_shop/my_app/views.py
+++ my_shop/my_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
-from rest_framework import generics, viewsets
-from .models import Product
-from .serializers import ProductSerializer
+from django.shortcuts import render
+from rest_framework import generics, viewsets, status
+from rest_framework.decorators import action
+from .models import Product, Comment
+from rest_framework.response import Response
+from .serializers import ProductSerializer, CommentSerializer
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -11,5 +12,15 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    @action(detail=True, methods=['post'])
+    def flag(self, request, pk=None):
+        comment = self.get_object()
+        comment.flagged = True
+        comment.save()
+        return Response(
+            {"status": "comment flagged"},
+            status=status.HTTP_200_OK
+        )
