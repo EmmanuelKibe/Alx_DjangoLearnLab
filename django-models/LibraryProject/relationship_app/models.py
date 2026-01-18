816649bcca070b5426759ad7a25f15837e2d5c25
Emmanuel <emmanuelkibe148@gmail.com>
2026-01-18 18:58:02 +0300

    extend books model to contain explicit permissions on who can perform certain functions

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -14,6 +14,13 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a new book entry"),
+            ("can_change_book", "Can edit existing book details"),
+            ("can_delete_book", "Can remove a book from the system"),
+        ]
+
     def __str__(self):
         return self.title
     
